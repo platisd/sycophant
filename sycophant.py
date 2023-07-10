f79d285fd7c3b5e8c2f4f015cdefc56156ec2c38
Dimitris Platis <dimitris.platis@neat.no>
2023-07-10 17:18:47 +0200

    Use exact time for post date

--- sycophant.py
+++ sycophant.py
@@ -282,7 +282,7 @@ def run(
     )
 
     post_title = "'" + final_article["title"] + "'"
-    post_date = current_date
+    post_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S %z")
     post_tags = generated_tags
     img_path = assets_dir
     post_image = image_file_name
